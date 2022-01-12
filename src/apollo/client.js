96a324d299d9b11751987b76aef9a91ea545d7e3
MoonwalkerSwap <7510709+MoonWalkerJim@users.noreply.github.com>
2022-01-12 20:04:28 +0000

    Update client.js

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.bscgraph.org/subgraphs/id/QmUDNRjYZ7XbgTvfVnXHj6LcTNacDD9GPXHWLjdTKi6om6',
+    uri: 'https://api.thegraph.com/subgraphs/name/moonwalkerjim/moonwalkerswapexchange',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
@@ -36,7 +36,7 @@ export const stakingClient = new ApolloClient({
 
 export const blockClient = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.bscgraph.org/subgraphs/name/bsc-blocks',
+    uri: 'https://api.thegraph.com/subgraphs/name/moonwalkerjim/blocks',
   }),
   cache: new InMemoryCache(),
 })
